4cda6b851d2a08f18374908e2a5072cae2b51c67
jane <m3281@hotmail.com>
2021-02-12 23:53:01 +0800

    simplify duplicate code wz let

--- bank_account_spec.rb
+++ bank_account_spec.rb
@@ -1,14 +1,13 @@
 require "./bank_account"
 RSpec.describe BankAccount do
+  let(:account) { BankAccount.new(10) }
   describe "deposit feature" do
     it "default amount to be 10 dollars, deposit 5 dollars in to the account and expect it to be $15 " do
-      account = BankAccount.new(10)
       account.deposit(5)
       expect(account.balance).to be 15
     end
 
     it "default amount to be 10 dollars, deposit -5 dollars and expect it to be $10" do
-      account = BankAccount.new(10)
       account.deposit(-5)
       expect(account.balance).to be 10
     end
@@ -16,7 +15,6 @@ RSpec.describe BankAccount do
 
   describe "withdraw feature" do
     it "default account balance to be $10, withdraw $5 , expect balance to be $5" do
-      account = BankAccount.new(10)
       amount =account.withdraw(5)
 
       expect(amount).to be 5
@@ -24,14 +22,12 @@ RSpec.describe BankAccount do
     end
     it "default account balance to be $10, withdraw $20, expect balance to be 
     $10 (unable to withdraw amount greater than original balance)" do
-      account = BankAccount.new(10)
       amount = account.withdraw(20)
 
       expect(amount).to be 0
       expect(account.balance).to be 10
     end
     it "default account balance to be $10, withdraw $-5, expect balance to be $10(unable to withdraw amount equals to or less than 0)" do
-      account = BankAccount.new(10)
       amount = account.withdraw(-5)
       
       expect(amount).to be 0
